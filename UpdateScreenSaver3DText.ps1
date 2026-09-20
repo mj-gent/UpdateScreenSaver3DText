77e37424595f37733ec59f617135dfc8961334f4
mj-gent <106419973+mj-gent@users.noreply.github.com>
2026-09-20 15:35:53 -0400

    Changed $games to $list
    New variable name is clearer to users, or at least more generic. My list happens to contain memorable video games.

--- UpdateScreenSaver3DText.ps1
+++ UpdateScreenSaver3DText.ps1
@@ -1,18 +1,19 @@
 # Check current value, so that it won't be picked twice in a row
 $check = Get-ItemPropertyValue -Path "HKCU:\SOFTWARE\Microsoft\Windows\CurrentVersion\Screensavers\ssText3d" -Name "DisplayString"
 
-# Initialize array of choices from list, then chooses one   
-$games = Get-Content -Path "E:\Writing & Documents\MISC\FAVGAMES.txt" # <-------- Change this path to wherever YOUR text file is stored.
-$choice = Get-Random $games
+# Initialize array of choices from list, then chooses randomly
+$list = Get-Content -Path "E:\Writing & Documents\MISC\FAVGAMES.txt" # <-------- Change this path to wherever YOUR text file is stored.
+$choice = Get-Random $list
 
-# Remove duplicate from pool and choose again.
+# If first choice matches current value, remove from pool and choose randomly again.
 if($choice -eq $check)
 {
-    $games.Replace($choice, "")
-    $choice = Get-Random $games
+    $list.Replace($choice, "")
+    $choice = Get-Random $list
 }
 
-# Check number of characters, shortening excessively long entries. Any strings longer than 20 characters will cause the text to default to something like "Windows 11"
+# Check number of characters, shortening excessively long entries and appending "--".
+# Any strings longer than 20 characters will cause the text to default to something like "Windows 11"
 if($choice.Length -gt 20)
 {
     $choice = $choice.Substring(0, 18) + "--"
